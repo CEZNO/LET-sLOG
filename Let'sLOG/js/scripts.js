774d76efcaaa47b329731ea61ff99074d12a4b6c
CEZNO <105482844+CEZNO@users.noreply.github.com>
2022-09-29 16:40:53 +0900

    Add files via upload

--- Let'sLOG/js/scripts.js
+++ Let'sLOG/js/scripts.js
@@ -1,41 +1,52 @@
 /*!
-* Start Bootstrap - Clean Blog v6.0.8 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
-        }
-        scrollPos = currentTop;
-    });
-})
+ * Start Bootstrap - Clean Blog v6.0.8 (https://startbootstrap.com/theme/clean-blog)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+ */
+window.addEventListener("DOMContentLoaded", () => {
+  let scrollPos = 0;
+  const mainNav = document.getElementById("mainNav");
+  const headerHeight = mainNav.clientHeight;
+  window.addEventListener("scroll", function () {
+    const currentTop = document.body.getBoundingClientRect().top * -1;
+    if (currentTop < scrollPos) {
+      // Scrolling Up
+      if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
+        mainNav.classList.add("is-visible");
+      } else {
+        console.log(123);
+        mainNav.classList.remove("is-visible", "is-fixed");
+      }
+    } else {
+      // Scrolling Down
+      mainNav.classList.remove(["is-visible"]);
+      if (
+        currentTop > headerHeight &&
+        !mainNav.classList.contains("is-fixed")
+      ) {
+        mainNav.classList.add("is-fixed");
+      }
+    }
+    scrollPos = currentTop;
+  });
+});
 
-let inputDataCheck = (id) => {
-  if (id) {            
-      $("#" + id).removeClass("is-invalid");
-  }
-}
+document.addEventListener(
+  "keydown",
+  function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  },
+  true
+);
 
-function minLengthCheck(object) {
-  if (object.value.length < object.minLength) {
-      object.value = object.value.slice(0, object.minLength);
+function form_check() {
+  var password = document.getElementById("password");
+  var cofm_password = document.getElementById("confirm_password");
+
+  if (cofm_password.value !== password.value) {
+    cofm_password.className = "form-control is-invalid"
+    return false;
   }
 }
